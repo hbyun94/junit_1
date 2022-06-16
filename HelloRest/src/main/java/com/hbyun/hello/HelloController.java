fd46e03db5c4d2205446646b8f062159d5693f97
nsntk <nsntk@DESKTOP-TIS3V47>
2022-06-16 16:17:35 +0900

    junit 입문 및 깃허브 최초 커밋

---
+++ HelloRest/src/main/java/com/hbyun/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.hbyun.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String hi() {
+		return "hi there~";
+	}
+}
---
+++ HelloRest/src/test/java/com/hbyun/hello/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.hbyun.hello;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringRunner.class)
+// JUnit에 내장된 실행자 외에 다른 실행자를 실행시키는데, 여기선 SpringRunner 실행자를 사용. 스프링부터 테스트와 JUnit 사이에 연결자 역할
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+	@Autowired(required = true)
+	private MockMvc mvc;
+	
+	@Test
+	void contextLoads() throws Exception {
+		String shouldReturn = "hi there~";
+		
+		mvc.perform(get("/hello"))
+		.andExpect(status().isOk())
+		.andExpect(content().string(shouldReturn));
+	}
+}
